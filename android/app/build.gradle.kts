88abce98d243185cf08c0d4c3370c1bc338b56b5
Rodrigo Lanes <rodrigolanes@gmail.com>
2025-12-17 17:40:17 -0300

    fix: corrige NDK e adiciona build_runner no workflow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "br.com.rodrigolanes.fipe"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
